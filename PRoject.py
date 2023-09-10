354dd9f962088c4c59910c5ac50091f06cb9d86c
SiberianCactus <kaktuz2101@mail.ru>
2023-09-10 16:25:02 +0700

    projectGBpythonГзв

--- PRoject.py
+++ PRoject.py
@@ -1,24 +1,18 @@
 import json
+from datetime import datetime
 
-def create_note():
-    title = input("Введите заголовок заметки: ")
-    content = input("Введите содержимое заметки: ")
-    note = {"title": title, "content": content}
-    return note
 
 def save_note(note):
     with open("notes.json", "a") as file:
         json.dump(note, file)
         file.write("\n")
 
-def read_notes():
-    with open("notes.json", "r") as file:
-        lines = file.readlines()
-        for line in lines:
-            note = json.loads(line)
-            print("Заголовок:", note["title"])
-            print("Содержимое:", note["content"])
-            print()
+def create_note():
+    title = input("Введите заголовок заметки: ")
+    content = input("Введите содержимое заметки: ")
+    created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    note = {"title": title, "content": content, "created_at": created_at, "updated_at": None}
+    return note
 
 def edit_note():
     title = input("Введите заголовок заметки, которую хотите отредактировать: ")
@@ -30,9 +24,20 @@ def edit_note():
             if note["title"] == title:
                 content = input("Введите новое содержимое заметки: ")
                 note["content"] = content
+                note["updated_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             json.dump(note, file)
             file.write("\n")
         file.truncate()
+
+def read_notes():
+    with open("notes.json", "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            note = json.loads(line)
+            print("Заголовок:", note["title"])
+            print("Содержимое:", note["content"])
+            print()
+
 
 def delete_note():
     title = input("Введите заголовок заметки, которую хотите удалить: ")
@@ -71,101 +76,3 @@ while True:
         break
     else:
         print("Неверный пункт меню. Попробуйте еще раз.")
-# def create_note():
-#     title = input("Введите заголовок заметки: ")
-#     content = input("Введите текст заметки: ")
-#     note = f"Заголовок: {title}\n\n{content}\n"
-#     return note
-
-# def save_note(note):
-#     filename = input("Введите имя файла для сохранения заметки: ")
-#     with open(filename, "w") as file:
-#         file.write(note)
-#     print(f"Заметка сохранена в файле {filename}")
-
-# def read_notes():
-#     files = os.listdir()
-#     note_files = [file for file in files if file.endswith(".txt")]
-#     if not note_files:
-#         print("Нет доступных заметок.")
-#         return
-#     print("Список доступных заметок:")
-#     for index, file in enumerate(note_files):
-#         print(f"{index + 1}. {file}")
-#     choice = int(input("Введите номер заметки для просмотра: "))
-#     if choice < 1 or choice > len(note_files):
-#         print("Некорректный выбор.")
-#         return
-#     with open(note_files[choice - 1], "r") as file:
-#         note = file.read()
-#     print("Содержимое заметки:")
-#     print(note)
-
-# def edit_note():
-#     files = os.listdir()
-#     note_files = [file for file in files if file.endswith(".txt")]
-#     if not note_files:
-#         print("Нет доступных заметок.")
-#         return
-#     print("Список доступных заметок:")
-#     for index, file in enumerate(note_files):
-#         print(f"{index + 1}. {file}")
-#     choice = int(input("Введите номер заметки для редактирования: "))
-#     if choice < 1 or choice > len(note_files):
-#         print("Некорректный выбор.")
-#         return
-#     with open(note_files[choice - 1], "r") as file:
-#         note = file.read()
-#     print("Содержимое заметки:")
-#     print(note)
-#     new_note = create_note()
-#     with open(note_files[choice - 1], "w") as file:
-#         file.write(new_note)
-#     print("Заметка успешно отредактирована.")
-
-# def delete_note():
-#     files = os.listdir()
-#     note_files = [file for file in files if file.endswith(".txt")]
-#     if not note_files:
-#         print("Нет доступных заметок.")
-#         return
-#     print("Список доступных заметок:")
-#     for index, file in enumerate(note_files):
-#         print(f"{index + 1}. {file}")
-#     choice = int(input("Введите номер заметки для удаления: "))
-#     if choice < 1 or choice > len(note_files):
-#         print("Некорректный выбор.")
-#         return
-#     os.remove(note_files[choice - 1])
-#     print("Заметка успешно удалена.")
-
-# def main():
-#     while True:
-#         print("\nМеню:")
-#         print("1. Создать заметку")
-#         print("2. Сохранить заметку")
-#         print("3. Просмотреть список заметок")
-#         print("4. Редактировать заметку")
-#         print("5. Удалить заметку")
-#         print("6. Выйти из программы")
-#         choice = input("Выберите действие: ")
-#         if choice == "1":
-#             note = create_note()
-#             print(note)
-#         elif choice == "2":
-#             note = create_note()
-#             save_note(note)
-#         elif choice == "3":
-#             read_notes()
-#         elif choice == "4":
-#             edit_note()
-#         elif choice == "5":
-#             delete_note()
-#         elif choice == "6":
-#             print("Программа завершена.")
-#             break
-#         else:
-#             print("Некорректный выбор.")
-
-# if __name__ == "__main__":
-#     main()
